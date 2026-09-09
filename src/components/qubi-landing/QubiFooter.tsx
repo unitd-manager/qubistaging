f913564b91c7c6a411e3ed73f7bca28e06591c95
mirzathasul-stack <mirzathasul@gmail.com>
2026-09-09 19:21:14 +0530

    LOGO change

--- src/components/qubi-landing/QubiFooter.tsx
+++ src/components/qubi-landing/QubiFooter.tsx
@@ -37,7 +37,7 @@ const QubiFooter = () => {
           </div>
         </div>
         <div className="copyright">
-          <span>© 2026 qubi by Qbotica.</span>
+  
           <span>Privacy Policy · Terms of Service</span>
         </div>
       </div>
